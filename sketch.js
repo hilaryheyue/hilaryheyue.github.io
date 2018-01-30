94a8e5f1bd3a6a0240580751f615543d5715d860
hilaryheyue <35782578+hilaryheyue@users.noreply.github.com>
2018-01-30 18:20:37 -0500

    week 2 add clock

--- sketch.js
+++ sketch.js
@@ -4,9 +4,12 @@ function setup() {
 
 function draw() {
   background ('black');
+  
   fill (255, 255, 255);
-  rect (220, 40, 3, 20);
+  rect (220, 40, map(second(),0,60,0,400), 20);
+  fill (255, 255, map(minute(),0,60,0,255));
   ellipse (210, 40, 5, 5);
+  fill (map(minute(),0,60,0,255), 255, 255);
   ellipse (210, 60, 5, 5);
   textSize (30);
   fill (236, 0, 139);
