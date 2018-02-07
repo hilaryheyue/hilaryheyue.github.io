9cc250fa5d849e48af998c528ceef4147d8ce252
hilaryheyue <35782578+hilaryheyue@users.noreply.github.com>
2018-02-07 02:12:02 -0500

    updated clock 2

--- A2/A2.2 Clock 2/sketch_A2.2 Clock 2.js
+++ A2/A2.2 Clock 2/sketch_A2.2 Clock 2.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(600, 600);
+  rectMode(CENTER)
 }
 
 function draw() {
@@ -14,9 +15,9 @@ function draw() {
     h=h-12
   }
 
-  var angles = map(second(), 0, 60, 0, TWO_PI) - 3*PI/4;
-  var anglem = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - 3*PI/4;
-  var angleh = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - 3*PI/4;
+  var angles = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  var anglem = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  var angleh = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
 
   //center
   var cx, cy;
@@ -31,18 +32,36 @@ function draw() {
   push();
   rotate (angleh);
   fill (128,130,132);
-  rect (0, 0, 150, 150);
+  rect (0, 0, 300, 300);
+  pop();
+
+  push();
+  rotate (angleh-PI/2)
+  fill ('black')
+  rect (0, 150, 25, 150);
   pop();
 
   push();
   rotate (anglem);
   fill (187,189,192);
-  rect (0, 0, 100, 100);
+  rect (0, 0, 200, 200);
+  pop();
+
+  push();
+  rotate (anglem-PI/2);
+  fill (128,130,132);
+  rect (0, 100, 15, 100);
   pop();
 
   push();
   rotate (angles);
   fill (241,241,242);
-  rect (0, 0, 50, 50);
+  rect (0, 0, 100, 100);
+  pop();
+
+  push();
+  rotate (angles-PI/2);
+  fill (187,189,192);
+  rect (0, 50, 5, 50);
   pop();
 }
