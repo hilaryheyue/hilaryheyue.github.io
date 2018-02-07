f92a5bd208a6e153a2bea1b24da88241f5f97c86
hilaryheyue <35782578+hilaryheyue@users.noreply.github.com>
2018-02-07 16:12:24 -0500

    A2.2 Clock3

--- A2/A2.2 Clock 3/sketch_A2.2 Clock 3.js
+++ A2/A2.2 Clock 3/sketch_A2.2 Clock 3.js
@@ -1,17 +1,35 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background ('black');
-  
+  noStroke();
+
+  	var s = second()
+	var m = minute()
+	var h = hour()
+
+  //Hr Rect
+  fill ('white');
+  text ('HR', 50, 160);
   fill (255, 255, 255);
-  rect (220, 40, map(second(),0,60,0,400), 20);
-  fill (255, 255, map(minute(),0,60,0,255));
-  ellipse (210, 40, 5, 5);
-  fill (map(minute(),0,60,0,255), 255, 255);
-  ellipse (210, 60, 5, 5);
-  textSize (30);
-  fill (236, 0, 139);
-  text ('hilary says hi', 20, 60);
+  rect (100, 120, map(hour(),0,24,0,450), 60);
+  text (h, map(hour(),0,24,0,450)+110, 160);
+
+  //Min Rect
+  fill ('white');
+  text ('MIN', 50, 300);
+  fill (255, 255, 255);
+  rect (100, 270, map(minute(),0,60,0,450), 60);
+  text (m, map(minute(),0,60,0,450)+110, 300);  
+
+  //Sec Rect
+  fill ('white');
+  text ('SEC', 50, 440);
+  fill (255, 255, 255);
+  rect (100, 410, map(second(),0,60,0,450), 60);
+  text (s, map(second(),0,60,0,450)+110, 440);  
+
+  
 }
