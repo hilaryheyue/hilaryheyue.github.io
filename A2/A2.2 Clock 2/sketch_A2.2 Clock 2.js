792c5cc171ce39bc3f06a4a2580a384117327c07
hilaryheyue <35782578+hilaryheyue@users.noreply.github.com>
2018-02-07 01:51:49 -0500

    Add A2.2 Clock 2

--- A2/A2.2 Clock 2/sketch_A2.2 Clock 2.js
+++ A2/A2.2 Clock 2/sketch_A2.2 Clock 2.js
@@ -1,17 +1,48 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background ('black');
-  
-  fill (255, 255, 255);
-  rect (220, 40, map(second(),0,60,0,400), 20);
-  fill (255, 255, map(minute(),0,60,0,255));
-  ellipse (210, 40, 5, 5);
-  fill (map(minute(),0,60,0,255), 255, 255);
-  ellipse (210, 60, 5, 5);
-  textSize (30);
-  fill (236, 0, 139);
-  text ('hilary says hi', 20, 60);
+
+  var h=hour();
+  var m=minute();
+  var s=second ();
+
+
+  if (h>=12) {
+    h=h-12
+  }
+
+  var angles = map(second(), 0, 60, 0, TWO_PI) - 3*PI/4;
+  var anglem = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - 3*PI/4;
+  var angleh = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - 3*PI/4;
+
+  //center
+  var cx, cy;
+
+  fill ('white');
+  stroke ('black');
+  text ('the time right now is : \n' + h + ':' + m + ':' + s, 100, 100);
+
+  translate (300,300);
+  noStroke ();
+ 
+  push();
+  rotate (angleh);
+  fill (128,130,132);
+  rect (0, 0, 150, 150);
+  pop();
+
+  push();
+  rotate (anglem);
+  fill (187,189,192);
+  rect (0, 0, 100, 100);
+  pop();
+
+  push();
+  rotate (angles);
+  fill (241,241,242);
+  rect (0, 0, 50, 50);
+  pop();
 }
